4934c6216caa13ba646a33920309c57b5b3e59e8
Lauren Bayldon Block <lblock2@stanford.edu>
2015-02-05 12:41:18 +0100

    csvflux1 working slightly; gets column

---
+++ csvflux1.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <set>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+#define kMaxInput 32
+#define kMaxCols 16
+#define kCatLine 3
+#define kDataLine 6
+#define kColWidth 2
+
+
+class FileReader
+{
+public:
+	 FileReader(char filename[]) : numcols(1) {
+	 	fp.open(filename, ios::in);
+	 	if(!fp) {
+	 		cout << "sorry, file not found" << endl;
+			return;
+	 	}
+	 	getNumCols();
+	 	// printFile();
+	 	skipLine();
+	 	createCategories();
+	 	DataColumn dc;
+	 	dc.nColumnsSkipped = 2;
+	 	getColumn(dc);
+	 }
+
+	~ FileReader() {
+		fp.close();
+	}
+
+	struct DataColumn
+	{
+		int nColumnsSkipped;
+		string name;
+		string category;
+	};
+
+	void getColumn(DataColumn dc) {
+		string line;
+		for(int i = 0; i < kDataLine; i++) skipLine();
+		while(!fp.eof()) {
+			for (int i = 0; i < dc.nColumnsSkipped; i++) getline(fp, line, ',');
+			for(int i = 0; i < kColWidth; i++) {
+				getline(fp, line, ',');
+				cout << line;
+			}
+			cout << endl;
+			for (int i = 0; i < (numcols - kColWidth - dc.nColumnsSkipped); i++) getline(fp, line, ',');
+		}
+
+	}
+
+
+	void printFile() {
+		string line;
+		int counter = 0;
+	 	while(!fp.eof()){
+	 		getline(fp, line, ',');
+	 		cout << line;
+	 		if (counter % numcols == 0) cout << endl;
+	 		if(counter++ > 488 * numcols) break;
+	 	}
+	}
+
+
+private:
+	vector<int> columnCommas;
+	vector<DataColumn> columns;
+	set<string> categoryNames;
+	int numcols;
+
+	//map<string, DataColumn> columnMap;
+	fstream fp;
+
+	struct Category
+	{
+		string name;
+		vector<DataColumn> catColumns;
+		
+	};
+
+	set<Category> allCats;
+
+	void skipLine() {
+		string tempLine;
+		for (int i = 0; i < numcols; i++) {
+			getline(fp, tempLine, ',');
+		}
+	}
+
+	void getNumCols() {
+		numcols = 0;
+	 	string line;
+	 	getline(fp, line, ',');
+	 	while (line.find("Fin") == string::npos) {
+	 		getline(fp, line, ',');
+	 		if (numcols++ > kMaxCols) break;
+	 	}
+	}
+
+	void createCategories() {
+		string line;
+		fp.clear();
+		fp.seekg(ios::beg);
+		for (int i = 0; i < kCatLine; i++) skipLine();
+		for(int i = 0; i < numcols; i++) {
+			getline(fp, line, ',');
+			// cout << line << endl;
+			if (line.length() > 0) {
+				if(categoryNames.find(line) == categoryNames.end()) categoryNames.insert(line);
+				else continue;
+				cout << line << endl;
+			}
+		}
+		cout << categoryNames.size() << endl;
+	}
+
+
+
+
+};
+
+
+int main() {
+	char filename[kMaxInput];
+	cout << "Filename: ";
+	cin.getline(filename, kMaxInput);
+	FileReader reader(filename);
+}
